a0abb35a0b64b719f1dcedd3d7d014fa2c041392
Hoangvm02 <hoangvmph13792@fpt.edu.vn>
2022-05-13 07:26:26 +0700

    chap02

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -15,11 +15,45 @@ export class AppComponent {
   students = [
     {
       name: 'Hoangvm',
-      id:'PH13792'
+      id:'PH13792',
+      status: 0
     },
     {
       name: 'Hoangvm2',
-      id:'PH13793'
+      id:'PH13793',
+      status: 1
+    },
+    {
+      name: 'Hoang',
+      id:'PH137111',
+      status: 1
+    }
+  ]
+
+  champs = [
+    {
+      name: 'zed',
+      dame: 50000,
+      defend: 40,
+      speed: 1000,
+      price:8000,
+      avata: "https://cdn.tgdd.vn/2020/09/content/galaxy-slayer-zed-splash-art-lol-uhdpaper.com-4K-64-800x450.jpg"
+    },
+    {
+      name: 'jax',
+      dame: 50000,
+      defend: 40,
+      speed: 1000,
+      price:8000,
+      avata: "https://phongvu.vn/lmht/wp-content/uploads/2018/08/jax-than.jpg"
+    },
+    {
+      name: 'Yasuo',
+      dame: 50000,
+      defend: 40,
+      speed: 1000,
+      price:8000,
+      avata: "https://cdngarenanow-a.akamaihd.net/webmain/static/pss/lol/items_splash/yasuo_17.jpg"
     }
   ]
   
--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ChampsComponent } from './champs/champs.component';
+import { TableComponent } from './table/table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChampsComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
