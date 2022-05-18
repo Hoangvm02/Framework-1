0ce6b8af323a672f8a4cb8db1d335ba05ab5628d
Hoangvm02 <hoangvmph13792@fpt.edu.vn>
2022-05-18 07:25:13 +0700

    submittt

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -24,18 +24,69 @@ export class AppComponent {
       defend: 40,
       speed: 1000,
       price:8000,
-      avata: "https://phongvu.vn/lmht/wp-content/uploads/2018/08/jax-than.jpg"
+      avata: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlNqDRXuh2gMzS77P4GLRbaq7_KuT2-fMHlXojTkVW5mNdNE3sWvPolZhXMFN3KjGu2nI&usqp=CAU"
     },
     {
       name: 'Yasuo',
       dame: 50000,
       defend: 40,
       speed: 1000,
-      price:8000,
+      price:3000,
       avata: "https://cdngarenanow-a.akamaihd.net/webmain/static/pss/lol/items_splash/yasuo_17.jpg"
     }
   ];
   studenName = 'Hoangvm';
   studentId = 'PH13792'
   
+  showStatus = true;
+  onClickBtn(){
+    console.log("Btn clicked!");
+    this.showStatus = !this.showStatus
+  }
+
+  onInput(event: any, inputName: String){
+    console.log(inputName,event.target.value);
+    
+  }
+  
+  inputValue = {
+    name: '',
+    avata: '',
+    speed: '',
+    price: '',
+    dame: '',
+    defend:''
+  }
+  OnInput(event: any, key: 'name' | 'avata'|'speed'|'price'|'dame'|'defend'){
+    this.inputValue[key] = event.target.value;
+  }
+
+  // OnInputName(event: any){
+  //   this.inputValue['name'] = event.target.value;
+    
+  // }
+  // OnInputAvatar(event: any){
+  // this.inputValue['avatar'] = event.target.value;
+    
+  // }
+  onClick(){
+    console.log("Gía trị obj các ô input", this.inputValue);
+
+    this.champs.push({
+      ...this.inputValue,
+      dame: +this.inputValue.dame,
+      defend: +this.inputValue.defend,
+      price: +this.inputValue.price,
+      speed: +this.inputValue.speed
+    });
+
+    this.inputValue = {
+      name: '',
+      avata: '',
+      dame: '',
+      defend: '',
+      speed: '',
+      price:''
+    }
+  }
 }
