cc0949ad7c87dcbaa976a455f807c2c2b1ed63c2
Hoangvmph13792 <93896285+Hoangvm02@users.noreply.github.com>
2022-05-21 07:59:04 +0700

    Delete Nodejs directory

--- Nodejs/routes/home.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-const route = express.Router();
-route.get('/', (request, response) =>{
-    response.send(`
-    <h1>Home Page</h1>
-    <p>Hello</p>
-    `);
-});
-export default route;
