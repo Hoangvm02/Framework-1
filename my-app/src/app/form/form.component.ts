912b6279ef8e5e3be714de921a0b8349be4fd3bc
Hoangvm02 <hoangvmph13792@fpt.edu.vn>
2022-05-21 07:56:49 +0700

    lab2

--- my-app/src/app/form/form.component.ts
+++ my-app/src/app/form/form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -11,9 +12,52 @@ export class FormComponent implements OnInit {
 
   ngOnInit(): void {
   }
-  onSubmit(formData: {name: string, age: number}){
-    console.log(formData);
-    
+  imputValues = {
+    id:0,
+    name: '',
+    age: 0,
+    email:'',
+    sdt: ""
   }
 
+  users = [
+    { 
+      id: 1,
+      name: 'Vũ Minh Hoàng',
+      age: 20,
+      email: 'hoangvmph13792@gmail.com',
+      sdt: "0913983123"
+    }
+  ]
+  onSubmit(userForm: NgForm ) {
+    const newUsers = this.users.map(user => user.id).sort((a: number, b: number) => b - a);
+
+    const maxId = newUsers[0]
+    // console.log(formData);
+
+    if(userForm.value.id === 0){
+      this.users.push({
+        ...userForm.value,
+        id: maxId + 1
+      });
+    } else{
+      this.imputValues=({
+        ...userForm.value,
+        id:this.imputValues.id
+      })
+    }
+    userForm.resetForm({
+      name:'',
+      age:0,
+      email: '',
+      sdt: ''
+    })
+  }
+  deleteId(id: number) {
+    this.users = this.users.filter(user => user.id !== id);
+  }
+  onEdit( user : any, id: number) {
+    this.imputValues= user.find((item:any)=>item.id === id)
+  }
+  
 }
