5373454443c4531a88a8bfe2bcd8bbc79014f070
Hoangvm02 <hoangvmph13792@fpt.edu.vn>
2022-06-01 09:10:29 +0700

    form-admin

---
+++ my-app/src/app/pages/admin/admin-product/admin-product-form/admin-product-form.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-admin-product-form',
+  templateUrl: './admin-product-form.component.html',
+  styleUrls: ['./admin-product-form.component.css']
+})
+export class AdminProductFormComponent implements OnInit {
+  productForm: FormGroup
+  constructor() { 
+    this.productForm = new FormGroup({
+      name: new FormControl('', Validators.required)
+    })
+  }
+
+  ngOnInit(): void {
+  }
+  onSubmit() {
+    console.log(this.productForm.value);
+    
+  }
+
+}
